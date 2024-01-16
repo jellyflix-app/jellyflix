bb726a2706933bbe45bca5f4781b0901bde9150d
jdk-21 <jdk-21@users.noreply.github.com>
2024-01-16 18:06:35 +0000

    deploy: 066209aa5ba7d79a94822b08ec64c6c2795fdc6b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "9b8b547c5feae309f602b06e5ab48d31",
-"index.html": "bebdb61709f26affcb43b269ebcbe642",
-"/": "bebdb61709f26affcb43b269ebcbe642",
+"index.html": "ad8bf58f071c77d6c2a50b417ea83f90",
+"/": "ad8bf58f071c77d6c2a50b417ea83f90",
 "assets/AssetManifest.bin": "20b67a13e12bf5bb74cbab4bc01d98ae",
 "assets/fonts/MaterialIcons-Regular.otf": "4d075b36b4eb98b7b0aef04775d77feb",
 "assets/AssetManifest.bin.json": "ffa18c6ec86f8085469bb3006c92f968",
