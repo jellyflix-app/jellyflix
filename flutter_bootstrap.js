d90ea4a04a74d807f04da98b07ea2af6368806fd
jdk-21 <jdk-21@users.noreply.github.com>
2024-05-19 17:35:45 +0000

    deploy: 9a15b486243b6a575bc54d823db65f68bc47f8b7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4070295470"
+    serviceWorkerVersion: "3167832275"
   }
 });
