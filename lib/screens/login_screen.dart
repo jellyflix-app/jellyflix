ab01f2c53ab025448188a9b28198ef811c7b2b80
RA341 <razerishi1505@gmail.com>
2024-09-01 23:24:47 -0400

    feat: added logging in on pressing enter

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -20,129 +21,162 @@ class LoginScreen extends HookConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-          child: SingleChildScrollView(
-        child: SizedBox(
-          width: 400,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: AutofillGroup(
-              child: Column(
-                children: [
-                  Text(AppLocalizations.of(context)!.appName,
-                      style: Theme.of(context).textTheme.displaySmall),
-                  Text(
-                    AppLocalizations.of(context)!.appSubtitle,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: TextField(
-                      controller: serverAddress,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        labelText: AppLocalizations.of(context)!.serverAddress,
-                        hintText: 'http://',
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: TextField(
-                      controller: userName,
-                      autofillHints: const [AutofillHints.username],
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        labelText: AppLocalizations.of(context)!.username,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: TextField(
-                      obscureText: true,
-                      autofillHints: const [AutofillHints.password],
-                      controller: password,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        labelText: AppLocalizations.of(context)!.password,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: SizedBox(
-                      height: 45,
-                      width: 100,
-                      child: FilledButton(
-                        onPressed: () async {
-                          try {
-                            final missingFields = formatMissingFields(
-                              context,
-                              userName.text,
-                              password.text,
-                              serverAddress.text,
-                            );
-                            if (missingFields.isNotEmpty) {
-                              await showInfoDialog(
+      body: CallbackShortcuts(
+        bindings: <ShortcutActivator, VoidCallback>{
+          const SingleActivator(LogicalKeyboardKey.enter): () async {
+            print('pressing enter');
+            await login(
+              context,
+              ref,
+              username: userName.text,
+              serverAddress: serverAddress.text,
+              password: password.text,
+            );
+          }
+        },
+        child: FocusScope(
+          // needed for enter shortcut to work
+          autofocus: true,
+          child: Center(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: 400,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: AutofillGroup(
+                    child: Column(
+                      children: [
+                        Text(AppLocalizations.of(context)!.appName,
+                            style: Theme.of(context).textTheme.displaySmall),
+                        Text(
+                          AppLocalizations.of(context)!.appSubtitle,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: TextField(
+                            controller: serverAddress,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              labelText:
+                                  AppLocalizations.of(context)!.serverAddress,
+                              hintText: 'http://',
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: TextField(
+                            controller: userName,
+                            autofillHints: const [AutofillHints.username],
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              labelText: AppLocalizations.of(context)!.username,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: TextField(
+                            obscureText: true,
+                            autofillHints: const [AutofillHints.password],
+                            textInputAction: TextInputAction.go,
+                            controller: password,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              labelText: AppLocalizations.of(context)!.password,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: SizedBox(
+                            height: 45,
+                            width: 100,
+                            child: FilledButton(
+                              onPressed: () async => await login(
                                 context,
-                                Text(
-                                  AppLocalizations.of(context)!.emptyFields,
-                                ),
-                                content: Text(missingFields),
-                              );
-                              return;
-                            }
-
-                            User user = User(
-                              name: userName.text,
-                              password: password.text,
-                              serverAdress: serverAddress.text,
-                            );
-                            await ref.read(authProvider).login(user);
-                            if (context.mounted) {
-                              context.go(ScreenPaths.home);
-                            }
-                          } on DioException catch (e) {
-                            if (!context.mounted) return;
-                            await showInfoDialog(
-                              context,
-                              Text(
-                                AppLocalizations.of(context)!
-                                    .errorConnectingToServer,
+                                ref,
+                                username: userName.text,
+                                serverAddress: serverAddress.text,
+                                password: password.text,
                               ),
-                              content: e.response?.statusCode == null
-                                  ? Text(e.toString())
-                                  : Text(formatHttpErrorCode(e.response)),
-                            );
-                            return;
-                          } catch (e) {
-                            if (!context.mounted) return;
-                            await showInfoDialog(
-                              context,
-                              Text(AppLocalizations.of(context)!
-                                  .errorConnectingToServer),
-                              content: Text(e.toString()),
-                            );
-                            return;
-                          }
-                        },
-                        child: Text(AppLocalizations.of(context)!.login),
-                      ),
+                              child: Text(AppLocalizations.of(context)!.login),
+                            ),
+                          ),
+                        ),
+                        kIsWeb
+                            ? Text(AppLocalizations.of(context)!.webDemoNote)
+                            : const SizedBox(),
+                      ],
                     ),
                   ),
-                  kIsWeb
-                      ? Text(AppLocalizations.of(context)!.webDemoNote)
-                      : const SizedBox(),
-                ],
+                ),
               ),
             ),
           ),
         ),
-      )),
+      ),
     );
+  }
+
+  Future<void> login(
+    BuildContext context,
+    WidgetRef ref, {
+    required String serverAddress,
+    required String username,
+    required String password,
+  }) async {
+    try {
+      final missingFields = formatMissingFields(
+        context,
+        username,
+        password,
+        serverAddress,
+      );
+      if (missingFields.isNotEmpty) {
+        await showInfoDialog(
+          context,
+          Text(
+            AppLocalizations.of(context)!.emptyFields,
+          ),
+          content: Text(missingFields),
+        );
+        return;
+      }
+
+      User user = User(
+        name: username,
+        password: password,
+        serverAdress: serverAddress,
+      );
+      await ref.read(authProvider).login(user);
+      if (context.mounted) {
+        context.go(ScreenPaths.home);
+      }
+    } on DioException catch (e) {
+      if (!context.mounted) return;
+      await showInfoDialog(
+        context,
+        Text(
+          AppLocalizations.of(context)!.errorConnectingToServer,
+        ),
+        content: e.response?.statusCode == null
+            ? Text(e.toString())
+            : Text(formatHttpErrorCode(e.response)),
+      );
+      return;
+    } catch (e) {
+      if (!context.mounted) return;
+      await showInfoDialog(
+        context,
+        Text(AppLocalizations.of(context)!.errorConnectingToServer),
+        content: Text(e.toString()),
+      );
+      return;
+    }
   }
 
   Future<void> showInfoDialog(
@@ -152,17 +186,27 @@ class LoginScreen extends HookConsumerWidget {
   }) async {
     await showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: title,
-        content: content,
-        actions: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Ok'),
-          )
-        ],
+      builder: (context) => CallbackShortcuts(
+        bindings: <ShortcutActivator, VoidCallback>{
+          const SingleActivator(LogicalKeyboardKey.enter): () {
+            Navigator.pop(context);
+          }
+        },
+        child: FocusScope(
+          autofocus: true,
+          child: AlertDialog(
+            title: title,
+            content: content,
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Ok'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
