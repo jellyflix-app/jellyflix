e3b7286f516f0b37846c344d8af3710d11965a21
RA341 <razerishi1505@gmail.com>
2024-09-02 22:41:16 -0400

    cleanup

--- lib/components/profile_card.dart
+++ lib/components/profile_card.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:jellyflix/models/screen_paths.dart';
 import 'package:jellyflix/providers/auth_provider.dart';
 
 class ProfileCard extends ConsumerWidget {
